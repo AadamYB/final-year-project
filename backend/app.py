152b35a66275542982c16380e156aa9a3721ae5c
Aadam Yiadom Boakye <aadam.y.boakye@gmail.com>
2025-02-15 01:49:32 +0000

    Set up webhook handler for github events

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,21 @@
 from flask import Flask
+from flask import request
+from flask import json
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
+def api_root():
     return "Server is running and receiving events!"
 
+@app.route('/events', methods = ['POST'])
+def api_events():
+    # event_data = request.get_json()
+    # return json.jsonify({"message": "Webhook received!"}), 200
+    if request.headers['Content-Type'] == 'application/json':
+        event_data = request.get_json()
+        print(event_data)
+        return json.jsonify(event_data), 200
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
