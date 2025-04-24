3a64e8925f6a7d0259e1d9c6617916cc932fcb3b
Aadam <ec221024@qmul.ac.uk>
2025-04-24 20:07:02 +0100

    Run tests and incorporate formatter/linting

--- backend/app.py
+++ backend/app.py
@@ -21,6 +21,7 @@ def api_events():
     repo_url = repo.get('clone_url')
     local_repo_path = os.path.join(REPO_DIRECTORY, repo_title.replace("/", "_"))
 
+    # Handles Pull Request Events
     if 'pull_request' in event:
         pr = event['pull_request']
         pr_branch = pr.get('head', {}).get('ref')
@@ -36,10 +37,17 @@ def api_events():
             # Then checkout the PR branch
             checkout_branch(local_repo_path, pr_branch)
 
-            # # Trigger Build & Test
+            # We want to make sure that the code passes the formatting before building the project
+            lint_project(local_repo_path)
+            format_project(local_repo_path)
+            
+            # Trigger the buiild and testing of the project
             build_project(local_repo_path)
+            run_tests(local_repo_path)
+
             return json.dumps({"status": "PR processed"}), 200
 
+    # Handles Push events
     elif 'pusher' in event and 'ref' in event:
         push_branch = event.get('ref').split("/")[-1]
 
@@ -47,15 +55,30 @@ def api_events():
 
         if repo_url and push_branch:
 
+            # First we clone the repository if it does not already exist, if so then pull changes
             clone_or_pull(repo_url, local_repo_path)
 
-
+            # Then checkout the PR branch
             checkout_branch(local_repo_path, push_branch)
+
+            # We want to make sure that the code passes the formatting before building the project
+            lint_project(local_repo_path)
+            format_project(local_repo_path)
+
+            # Trigger the buiild and testing of the project
             build_project(local_repo_path)
+            run_tests(local_repo_path)
+
             return json.dumps({"status": "Push processed"}), 200
 
     print("Unsupported or unknown event.")
     return json.dumps({"message": "Unsupported or unknown event type"}), 400
+
+
+# ---------------------------------------------------------------------------------------------
+# ------------------------------------ Utility Functions --------------------------------------
+# ---------------------------------------------------------------------------------------------
+
 
 def clone_or_pull(repo_url, local_repo_path):
     """ Clones a GitHub repository to a local directory and/or 
@@ -87,6 +110,54 @@ def build_project(local_repo_path):
         print("✅ Hooray! Build Successful!")
     except subprocess.CalledProcessError as e:
         print(f"❌ Failed to build the project. Error: {e}")
+
+def run_tests(local_repo_path):
+    """ Runs the test scripts for the user project """
+
+    print(f"Running tests for {local_repo_path}")
+    try:
+        subprocess.run(["docker", "run", "--rm", "project-image", "python3", "-m", "unittest", "discover", "-s", "tests"], check=True)
+        print("✅ All tests passed!")
+    except subprocess.CalledProcessError as e:
+        print(f"❌ Some tests failed. Error: {e}")
+
+def lint_project(local_repo_path):
+    """ Linting utility function inside the Docker container~ SWAG """
+    print("🔍 Running pylint checks...")
+
+    result = subprocess.run(
+        ["pylint", f"{local_repo_path}/app/main.py", "--rcfile", f"{local_repo_path}/.pylintrc"],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        text=True
+    )
+
+    print(result.stdout)
+
+    # Fail the pipeline run if score is too low
+    if "Your code has been rated at" in result.stdout:
+        score_line = [line for line in result.stdout.splitlines() if "Your code has been rated at" in line]
+        if score_line:
+            score = float(score_line[0].split(" ")[6].split("/")[0])
+            if score < 8.0:
+                raise Exception(f"❌ Lint failed! Score: {score}/10")
+            
+def format_project(local_repo_path):
+    """Formats code using Black formatter inside the Docker container."""
+    print("💅 Running black formatter...")
+
+    try:
+        subprocess.run([
+            "docker", "run", "--rm",
+            "-v", f"{local_repo_path}:/app",
+            "project-image",
+            "black", "app", "tests"
+        ], check=True)
+        print("✅ Code formatted successfully.")
+    except subprocess.CalledProcessError as e:
+        print(f"❌ Black formatter failed. Error: {e}")
+
+# ---------------------------------------------------------------------------------------------
 
 if __name__ == '__main__':
     os.makedirs(REPO_DIRECTORY, exist_ok=True)
